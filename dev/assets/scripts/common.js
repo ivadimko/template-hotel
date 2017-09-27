c76a80ff805839c8b78641bc1c10cddb81887e28
Vadim Ilchenko <vvadimko@gmail.com>
2017-09-27 19:33:53 +0300

    add files

--- dev/assets/scripts/common.js
+++ dev/assets/scripts/common.js
@@ -1,9 +1,44 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 mobileNav();
-});
-window.onload = function() {
+	let tabHeading = $('.tab__heading'),
+			tabText = $('.tab__text'),
+			tabImage = $('.tab__image');
+	$(tabHeading).eq(1).addClass('active');
+	$(tabText).eq(1).addClass('active');
+	$(tabImage).eq(1).addClass('active');
 
-};
+	tabHeading.on('click', function() {
+		if ($(this).hasClass('active')) {} else {
+			$(tabHeading).removeClass('active');
+			$(this).addClass('active');
+			$(tabImage).removeClass('inactive');
+			$('.header__images').find('.active').removeClass('active').addClass('inactive').fadeOut();
+			$(tabImage).removeClass('active');
+			$(tabImage).eq($(this).index()).addClass('active').fadeIn();
+			$(tabText).removeClass('active');
+			$(tabText).fadeOut().eq($(this).index()).fadeIn();
+			$(tabText).eq($(this).index()).addClass('active');
+		}
+	});
+	$('.gallery').slick({
+   	infinite: true,
+  	slidesToShow: 3,
+  	slidesToScroll: 2,
+  	autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  });
+	$('.item__img').hover(
+		function(){
+			$(this).parent().addClass('active');
+	},
+		function(){
+			$(this).parent().removeClass('active');
+		});
+
+
+});
+
 //Mobile menu function
 function mobileNav() {
 	var menu 			= document.querySelector('.header__menu'),
